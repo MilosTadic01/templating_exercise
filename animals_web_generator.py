18dbecf82be83cd4c1ff078070fe1926ae836a39
Milos Tadic <milosgtadic@yahoo.com>
2024-06-09 23:13:39 +0200

    set up for api_zootopia continuation

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,80 +1,57 @@
-import json
+import data_fetcher
 
 
-def load_data(file_path):
-    """Deserialize a JSON file"""
-    with open(file_path, "r", encoding='utf-8') as fd:
-        return json.load(fd)
+DST_HTML_FILE = 'animals.html'
 
 
 def put_to_html(select_data):
     """Replace placeholder in template file with serialized data, save new
-    file title 'animals_populated.html'"""
+    file as DST_HTML_FILE"""
     with open("animals_template.html", "r", encoding="utf-8") as fd:
         html_str = fd.read()
     if "__REPLACE_ANIMALS_INFO__" in html_str:
         html_str = html_str.replace("__REPLACE_ANIMALS_INFO__", select_data)
-        with open("animals_populated.html", "w", encoding="utf-8") as fd:
+        with open(DST_HTML_FILE, "w", encoding="utf-8") as fd:
             fd.write(html_str)
 
 
-def serialize_animal(fox):
+def serialize_animal(animal):
     """Format extracted data into list items 'cards'."""
     select_data = ''
     select_data += '<li class="cards__item">\n'
-    if 'name' in fox:
-        select_data += f'<div class="card__title">{fox["name"]}</div>\n'
+    if 'name' in animal:
+        select_data += f'<div class="card__title">{animal["name"]}</div>\n'
     select_data += '<p class="card__text">\n'
     select_data += '<ul class="info_items">'
-    if 'characteristics' in fox and 'diet' in fox['characteristics']:
+    if 'characteristics' in animal and 'diet' in animal['characteristics']:
         select_data += f'<li class="info_itm_li"><strong>Diet: </strong>'
-        select_data += f"{fox['characteristics']['diet']}</li>\n"
-    if 'locations' in fox and len(fox['locations']) > 0:
+        select_data += f"{animal['characteristics']['diet']}</li>\n"
+    if 'locations' in animal and len(animal['locations']) > 0:
         select_data += f'<li class="info_itm_li"><strong>Location: </strong>'
-        select_data += f"{fox['locations'][0]}</li>\n"
-    if 'characteristics' in fox and 'type' in fox['characteristics']:
+        select_data += f"{animal['locations'][0]}</li>\n"
+    if 'characteristics' in animal and 'type' in animal['characteristics']:
         select_data += f'<li class="info_itm_li"><strong>Type: </strong>'
-        select_data += f"{fox['characteristics']['type']}</li>\n"
+        select_data += f"{animal['characteristics']['type']}</li>\n"
     select_data += '</ul>'
     select_data += '</p></li>\n'
     return select_data
 
 
-def get_filtered_animals_data(animals_data, skin_type):
-    """Concat serialized data for animals that match user criterion"""
+def get_filtered_animals_data(animals_data, usr_input):
+    """Concat serialized data for animals that match user input"""
     select_data = ''
-    for fox in animals_data:
-        if ("skin_type" in fox["characteristics"] and
-                fox["characteristics"]["skin_type"] == skin_type):
-            select_data += serialize_animal(fox)
+    for animal in animals_data:
+        if ("name" in animal and
+                usr_input.lower() in animal["name"].lower()):
+            select_data += serialize_animal(animal)
     return select_data.strip()
-
-
-def get_user_input(animals_data):
-    """Create a set for user to choose from and get their input. If an
-    animal is missing the 'skin_type' key, it is not considered a candidate
-    because it can not match the user's criterion."""
-    skin_types = set()
-    for fox in animals_data:
-        if "skin_type" in fox["characteristics"]:
-            skin_types.add(fox["characteristics"]["skin_type"])
-    print("\nWe will only include animals with the skin type of your choice.")
-    print("Skin types present in dataset:")
-    for item in skin_types:
-        print(f'- {item}')
-    while True:
-        skin_type = input("Enter your choice (mind the caps): ").strip()
-        if skin_type in skin_types:
-            break
-        print("That skin type was not listed, try again.")
-    return skin_type
 
 
 def main():
     """Control flow"""
-    animals_data = load_data('animals_data.json')
-    skin_type = get_user_input(animals_data)
-    select_data = get_filtered_animals_data(animals_data, skin_type)
+    usr_input = input("Enter name of animal to produce a webpage about: ")
+    animals_data = data_fetcher.fetch_data(usr_input)
+    select_data = get_filtered_animals_data(animals_data, usr_input)
     put_to_html(select_data)
 
 
---
+++ data_fetcher.py
@@ -0,0 +1,29 @@
+import requests
+
+API_ENDPOINT_PREFIX = 'https://api.api-ninjas.com/v1/animals?name='
+API_KEY = 'a9gswUa/Srd5yh8f2zAQYg==vIQHjGXVE6AOGzpV'
+
+
+def fetch_data(animal_name):
+    """
+    Fetches the animals data for the animal 'animal_name'.
+    Returns: a list of animals, each animal is a dictionary:
+    {
+    'name': ...,
+    'taxonomy': {
+      ...
+    },
+    'locations': [
+      ...
+    ],
+    'characteristics': {
+      ...
+    }
+    },
+    """
+    url = API_ENDPOINT_PREFIX + animal_name + '&X-Api-Key=' + API_KEY
+    print(url)
+    animals_data = requests.get(url).json()
+    print(type(animals_data))
+    print(animals_data)
+    return animals_data
